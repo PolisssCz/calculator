27e4dbde7a24bdbae92cfa151c62be744b32a31c
Polisss <poli60@seznam.cz>
2022-03-14 11:50:53 +0100

    BUGFIX- závorky. Nyní lze počítat závorky klasickým způsobem např.(5x3)x10

--- assets/js/app.js
+++ assets/js/app.js
@@ -44,7 +44,7 @@ document.addEventListener('keypress', function (event) {
 $('button').on('click', function (event) { 
     event.preventDefault();
     var charKey = $(event.target).text();
-    
+
     // if the result is currently displayed, clear the screen.
     if( $('#equals').exists() ){
         screen.css({
@@ -55,15 +55,12 @@ $('button').on('click', function (event) {
         onScreen('', true);
     }
 
-    /* Button validation */
-    if ( $('#bracket-c').exists() ) {
-        return false
-    }
-
     // Checking the brackets-open
     if ( ( charKey == "(" ) )
-    {
-        if ( ($('#number-1').exists()) && ($('#operation').exists()) && (! $('#bracket-o').exists()) && (! $('#number-2').exists()) ) {
+    {   if ( (! $('#number-1').exists()) && (! $('#bracket-o').exists()) ) {
+            return onScreen(charKey);
+        }
+        else if ( ($('#number-1').exists()) && ($('#operation').exists()) && (! $('#bracket-o').exists()) && (! $('#number-2').exists()) ) {
             return onScreen(charKey);
         } else {
             return false
@@ -90,25 +87,36 @@ $('button').on('click', function (event) {
 
     if ( (charKey in operation) )
     {
-        if ( ($('#number-1').exists()) && (! $('#bracket-o').exists()) && (! $('#operation').exists()) ) { 
+        console.log("jsem tady: START");
+        if ( ($("#number-1").exists()) && (! $('#operation').exists()) || (! $('#operation').exists()) && ( $('#bracket-c').is(":last-child")) || ($("#bracket-number-1").exists()) && (! $("#bracket-operation").exists()) ) { 
             onScreen(charKey);
+            console.log("jsem tady:2");
             return true
         }
-        else if ( ($('#number-1').exists()) && ( $('#operation').exists()) && (! $('#bracket-number-1').exists()) ) 
+        else if ( ($('#number-1').exists()) && ( $('#operation').exists()) && (! $('#bracket-number-1').exists()) || ($('#operation').is(':last-child')) ) 
         {
             var operationChar = $('#operation').val();
             var original = screen.val();
             original.slice(0, -1) + ''+ operationChar +'';
+            console.log("jsem tady:3");
             return true
-        } else {
+        }  
+        else if ( ($('#bracket-o').is(':last-child')) ) 
+        {
+            return false
+        }
+         else {
             return false            
         }
-    }else{
+    } else if ( ($('#number-1').exists()) && ($('#bracket-c').is(':last-child')) ) {
+        return false     
+    }
+    else {
+        console.log("jsem tady:4");
         onScreen(charKey);
     }
 
 });
-
 /************
 * Validation.
 * Save operation value == "÷,x,-,+".
@@ -121,9 +129,8 @@ $('.btn-operation').on('click', function (event){
     // If there is a bracket, we can insert an operation value
     if( ($("#bracket-number-1").exists()) && (! $("#bracket-operation").exists()) ){
         $('<input id="bracket-operation" class="inputs" name="bracket-operation" type="hidden" value="'+ operation +'">').appendTo(form);
-        onScreen(operation);
     }
-    else if ( (! $("#bracket-number-2").exists()) && ( $("#bracket-operation").exists()) )
+    else if ( ($("#bracket-operation").is(':last-child')) )
     {
         $('#bracket-operation').remove();
         $('<input id="bracket-operation" class="inputs" name="bracket-operation" type="hidden" value="'+ operation +'">').appendTo(form);
@@ -142,7 +149,7 @@ $('.btn-operation').on('click', function (event){
     };
 
     // Overwriting the operational value
-    if( ($('#operation').exists()) && (! $('#bracket-o').exists()) )
+    if( ($('#operation').is(':last-child')) )
     { 
         // Remove the previous value of the operation
         $('#operation').remove();
@@ -156,8 +163,8 @@ $('.btn-operation').on('click', function (event){
         var correction = original.slice(0, -1) + ''+ operation +'';
         onScreen(correction, true)
         return true 
-    } 
-     else if ( ($('#number-1').exists()) && (! $('#bracket-o').exists()) ) 
+    }
+    else if ( ($("#number-1").exists()) && (! $('#operation').exists()) || (! $('#operation').exists()) && ( $('#bracket-c').is(":last-child")) )
     {// If we do not override the operational value 
         $('<input id="operation" class="inputs" name="operation" type="hidden" value="'+ operation +'">').appendTo(form);
         return true
@@ -208,7 +215,12 @@ inpSubmit.on('click', function (event) {
 $('#bracket-open').on('click', function bracket(event) {
     var bracketO = $(event.target).text();
 
-    if ( ($('#bracket-o').exists()) || ($('#number-2').exists()) || (! $('#operation').exists()) ) { 
+    if ( (! $('#number-1').exists()) && (! $('#bracket-o').exists()) ) { 
+        $('<input id="bracket-o" class="inputs" name="bracket-o" type="hidden" value="'+ bracketO +'">').appendTo(form);
+        return true
+    }
+    else if ( ($('#bracket-o').exists()) || ($('#number-2').exists()) || (! $('#operation').exists()) ) 
+    { 
         return false
     } else {
         $('<input id="bracket-o" class="inputs" name="bracket-o" type="hidden" value="'+ bracketO +'">').appendTo(form);
@@ -238,7 +250,7 @@ number.on('click', function (event) {
     if ( ($('#bracket-o').exists()) && (! $('#bracket-number-1').exists())  ) {
             $('<input id="bracket-number-1" class="inputs" name="bracket-number-1" type="hidden" value="'+ number +'">').appendTo(form);
         return true
-    }else if ( ($('#bracket-o').exists()) && ($('#bracket-number-1').exists()) && (! $('#bracket-operation').exists())) {
+    }else if ( ($('#bracket-o').exists()) && ($('#bracket-number-1').exists()) && (! $('#bracket-operation').exists()) ) {
         var firstPart = $('#bracket-number-1').val();
         $("#bracket-number-1").remove();
         $('<input id="bracket-number-1" class="inputs" name="bracket-number-1" type="hidden" value="'+ firstPart +''+ number +'">').appendTo(form);
@@ -257,6 +269,17 @@ number.on('click', function (event) {
     /* 
     *** Storing number no brackets
     */
+    if (! $('#number-1').exists() ) {
+        if ( ($('#bracket-o').exists()) && ($('#operation').exists()) ) {
+            $('<input id="number-1" class="inputs" name="number-1" type="hidden" value="'+ number +'">').appendTo(form);
+            return true
+        }
+        else if ( ($('#bracket-o').exists()) && ($('#bracket-number-1').exists()) ) 
+        {
+            return false
+        }
+    }
+
     if ( ($('#operation').exists()) && (! $('#bracket-number-1').exists()) && (! $('#number-2').exists()) ) { 
         $('<input id="number-2" class="inputs" name="number-2" type="hidden" value="'+ number +'">').appendTo(form);
     } 
@@ -265,17 +288,23 @@ number.on('click', function (event) {
         var firstPart = $('#number-2').val();
         $('#number-2').remove();
         $('<input id="number-2" class="inputs" name="number-2" type="hidden" value="'+ firstPart +''+ number +'">').appendTo(form);
-    }  
-    else if ( $('#number-1').exists() )
+    }else if ( $('#number-1').exists() )
     {
-        if ( $('#bracket-number-1').exists() ) {
-            return false
+        if ( $('#bracket-o').exists() )
+        {
+            if ( ($('#number-1').is(':last-child')) ) {
+                var firstPart = $('#number-1').val();
+                $('#number-1').remove();
+                $('<input id="number-1" class="inputs" name="number-1" type="hidden" value="'+ firstPart +''+ number +'">').appendTo(form);    
+            } else {
+                return false
+            }
         } 
         else if ( (! $('#bracket-number-1').exists()) && ( $('#number-1').exists() ) ) 
         {
             var firstPart = $('#number-1').val();
             $('#number-1').remove();
-            $('<input id="number-1" class="inputs" name="number-1" type="hidden" value="'+ firstPart +''+ number +'">').appendTo(form);         
+            $('<input id="number-1" class="inputs" name="number-1" type="hidden" value="'+ firstPart +''+ number +'">').appendTo(form);   
         };
     } else {
         $('<input id="number-1" class="inputs" name="number-1" type="hidden" value="'+ number +'">').appendTo(form);
@@ -285,20 +314,24 @@ number.on('click', function (event) {
 
 // Clear backspace
 $('#backspace').on('click', function clear(){
-    var input = $('.inputs:last-child');
-    var inputVal = $('.inputs:last-child').val().slice(0, -1);
-
-    // Deletion of data for calculation
-    if ( input.val().length > 1 ) {
-        input.appendTo(form).val(inputVal);
+    if ( $(".inputs").exists() ) {
+        var input = $('.inputs:last-child');
+        var inputVal = $('.inputs:last-child').val().slice(0, -1);
+    
+        // Deletion of data for calculation
+        if ( input.val().length > 1 ) {
+            input.appendTo(form).val(inputVal);
+        } else {
+            input.remove();
+        }
+    
+        // Visual display
+        var original = screen.val();
+        var newVal = original.slice(0, -1);
+        onScreen(newVal, true);
     } else {
-        input.remove();
+        return false
     }
-
-    // Visual display
-    var original = screen.val();
-    var newVal = original.slice(0, -1);
-    onScreen(newVal, true);
 
 });
 
